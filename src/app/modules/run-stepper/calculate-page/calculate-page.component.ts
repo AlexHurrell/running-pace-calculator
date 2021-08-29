36c552edbfd46c72d01b7bb108ca45833d58087c
AlexHurrell <39062336+AlexHurrell@users.noreply.github.com>
2021-08-29 21:30:28 +0200

    calculate pace

--- src/app/modules/run-stepper/calculate-page/calculate-page.component.ts
+++ src/app/modules/run-stepper/calculate-page/calculate-page.component.ts
@@ -1,10 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Choices } from '../run-selection-page/run-selection-page.component';
 
 interface distance {
   name: string;
   length: number;
 }
+
+interface calculationForm {
+  units: string;
+  time: {
+    hours: string;
+    minutes: string;
+    seconds: string;
+  };
+  pace: {
+    minutes: string;
+    seconds: string;
+  };
+  customDistance: string;
+  classicDistance: string;
+}
+
+type calculationFormControls = {
+  [key in keyof calculationForm]: AbstractControl;
+};
+type calculationFormGroup = FormGroup & {
+  value: calculationForm;
+  controls: calculationFormControls;
+};
 
 @Component({
   selector: 'app-calculate-page',
@@ -12,6 +43,8 @@ interface distance {
   styleUrls: ['./calculate-page.component.scss'],
 })
 export class CalculatePageComponent implements OnInit {
+  form: FormGroup;
+
   distances: distance[] = [
     {
       name: '5k',
@@ -35,12 +68,88 @@ export class CalculatePageComponent implements OnInit {
     },
   ];
 
-  km = false;
+  choices = Choices;
 
-  constructor(private route: ActivatedRoute) {}
+  choice: Choices | null | string;
+
+  result: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    console.log(id);
+    this.choice = this.route.snapshot.paramMap.get('id');
+    if (!this.choice) this.choice = Choices.Pace;
+
+    if (
+      this.choice !== Choices.Pace &&
+      this.choice !== Choices.Time &&
+      this.choice !== Choices.Distance
+    ) {
+      this.choice = Choices.Pace;
+    }
+
+    this.form = this.formBuilder.group({
+      units: ['km', Validators.required],
+      customDistance: ['', Validators.required],
+      classicDistance: '',
+      time: this.formBuilder.group(
+        {
+          hours: '',
+          minutes: '',
+          seconds: '',
+        },
+        { validator: this.oneRequired(Choices.Distance) }
+      ),
+      pace: this.formBuilder.group(
+        {
+          paceMinutes: '',
+          paceSeconds: '',
+        },
+        { validator: this.oneRequired(Choices.Pace) }
+      ),
+    });
+
+    this.form.valueChanges.subscribe((formValue) => {
+      if (this.form.valid) {
+        if (this.choice === Choices.Pace) {
+          this.result = this.paceResult(formValue);
+        } else if (this.choice === Choices.Distance) {
+        } else {
+        }
+      } else {
+        this.result = '';
+      }
+    });
+  }
+
+  oneRequired(choice: Choices) {
+    return (formGroup: FormGroup) => {
+      if (choice === this.choice) return null;
+      return Object.keys(formGroup.value).some((key) => !!formGroup.value[key])
+        ? null
+        : { oneRequired: '' };
+    };
+  }
+
+  private paceResult(formValue: calculationForm): string {
+    const seconds =
+      Number(formValue.time.hours) * 3600 +
+      Number(formValue.time.minutes) * 60 +
+      Number(formValue.time.seconds);
+    let pace = seconds / Number(formValue.customDistance);
+
+    let paceHours = String(Math.floor(pace / 3600)).padStart(2, '0');
+    pace %= 3600;
+    let paceMinutes = String(Math.floor(pace / 60)).padStart(2, '0');
+    let paceSeconds = String(Math.round(pace % 60)).padStart(2, '0');
+
+    return (
+      (paceHours ? paceHours + ':' : '') +
+      (paceMinutes ? paceMinutes + ':' : '') +
+      (paceSeconds ? paceSeconds : '')
+    );
   }
 }
--- src/app/modules/run-stepper/run-selection-page/run-selection-page.component.ts
+++ src/app/modules/run-stepper/run-selection-page/run-selection-page.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-enum Choices {
+export enum Choices {
   Distance = 'distance',
   Time = 'time',
   Pace = 'pace',
